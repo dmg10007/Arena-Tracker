9d4237a414b7247ea0c2843ac3ccc103e75343ce
dmg10007 <dalton.gifford@uconn.edu>
2026-03-29 07:37:47 -0400

    Add ErrorBoundary component

---
+++ src/components/ErrorBoundary.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+  componentDidCatch(error, info) {
+    // TODO: send to logging endpoint if available
+    // console.error('ErrorBoundary caught', error, info);
+  }
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ padding: 'var(--space-5)' }}>
+          <h2>Something went wrong</h2>
+          <pre style={{ whiteSpace: 'pre-wrap' }}>{String(this.state.error)}</pre>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
